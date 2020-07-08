f2971307dea1f695ba0667327f2328475ee480e3
ryan diffenbaugh <ryandiffenbaugh@gmail.comgit config --global user. name Ryan-Diffgit config --global user.email ryandiffenbaugh@gmail.com>
2020-07-08 14:41:08 -0700

    Added HTML elements

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 // import functions and grab DOM elements
-
+const button = document.querySelector('#play-button');
+const current = document.querySelector('#current');
+const wins = document.querySelector('#wins');
+const losses = document.querySelector('#losses');
+const total = document.querySelector('#total');
 // initialize state
 
 // set event listeners to update state and DOM
