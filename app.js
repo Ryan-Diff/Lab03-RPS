478fb3e5b71f689384d2f0cf4e26d1ebf56403c7
ryan diffenbaugh <ryandiffenbaugh@gmail.comgit config --global user. name Ryan-Diffgit config --global user.email ryandiffenbaugh@gmail.com>
2020-07-08 15:49:16 -0700

    Added JS function

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import {getRandomThrow, checkResults} from './r-p-g';
 // import functions and grab DOM elements
 const button = document.querySelector('#play-button');
 const current = document.querySelector('#current');
@@ -5,5 +6,15 @@ const wins = document.querySelector('#wins');
 const losses = document.querySelector('#losses');
 const total = document.querySelector('#total');
 // initialize state
+const game = () => {
+    let pScore= 0;
+    let cScore= 0;
 
+let totalGuesses = 0;
 // set event listeners to update state and DOM
+button.addEventListener('click', () => {
+    
+
+
+
+});
