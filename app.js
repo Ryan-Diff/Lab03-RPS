82c94d3bfd46f04a8ed69ac7463725fb405256f3
ryan diffenbaugh <ryandiffenbaugh@gmail.comgit config --global user. name Ryan-Diffgit config --global user.email ryandiffenbaugh@gmail.com>
2020-07-09 08:11:54 -0700

    Added reset-button

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const winSpan = document.querySelector('#wins');
 const lossSpan = document.querySelector('#losses');
 const totalSpan = document.querySelector('#total');
 const drawSpan = document.querySelector('#draw');
+const resetButton = document.getElementById('reset-button');
 // initialize state
 let draw = 0;
 let wins = 0;
@@ -39,3 +40,15 @@ button.addEventListener('click', () => {
     drawSpan.textContent = `draw=${draw}`;
 
 });
+
+const resetPage = () => {
+    const userChoice = window.confirm('Are you sure?');
+
+    if (userChoice === true) {
+        document.location.reload();
+    } else {
+        return false;
+    }
+};
+
+resetButton.addEventListener('click', resetPage);
