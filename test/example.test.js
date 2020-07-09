ab0bcbf01aacd399227ecc36dd7e637b7ca30663
ryan diffenbaugh <ryandiffenbaugh@gmail.comgit config --global user. name Ryan-Diffgit config --global user.email ryandiffenbaugh@gmail.com>
2020-07-09 12:59:11 -0700

    Added test for checkResult function

--- test/example.test.js
+++ test/example.test.js
@@ -1,5 +1,5 @@
 // IMPORT MODULES under test here:
-import { getRandomThrow } from '../r-p-g.js';
+import { getRandomThrow, checkResults } from '../r-p-g.js';
 
 
 const test = QUnit.test;
@@ -12,6 +12,18 @@ test('it should take in a number and return that number squared', (assert) => {
     //Act 
     // Call the function you're testing and set the result to a const
     const actual = getRandomThrow();
+    //Assert
+    // Make assertions about what is expected versus the actual result
+    assert.equal(expected, actual === 'rock' || actual === 'paper' || actual === 'scissors');
+});
+
+test('Check result and compare to the random number', (assert) => {
+    //Arrange
+    // Set up your parameters and expectations
+    const expected = false;
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = checkResults();
     //Assert
     // Make assertions about what is expected versus the actual result
     assert.equal(expected, actual === 'rock' || actual === 'paper' || actual === 'scissors');
